c1a6234268c061deff0e247d21d9634f694cd53e
fairuzulum <usleunmdi32@gmail.com>
2025-07-03 09:53:06 +0700

    update pwa

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public", // Simpan service worker di folder public
+  register: true,
+  disable: process.env.NODE_ENV === "development", // Nonaktifkan PWA saat development
+});
 
 const nextConfig: NextConfig = {
+
+
   images: {
     remotePatterns: [
       {
@@ -13,4 +22,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
